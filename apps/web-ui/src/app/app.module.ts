c934e7e4cd81588630dbfdd663b328d0e9aa6a35
TemmuzBurakYavuzer <36274955+TemmuzBurakYavuzer@users.noreply.github.com>
2020-08-10 10:48:13 +0300

    teacher-course frontend not finished

--- apps/web-ui/src/app/app.module.ts
+++ apps/web-ui/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 
 //Layout Components
 import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
-import { CoreModule } from './core/core.module'
+import { CoreModule } from './core/core.module';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentLayoutComponent
+    ContentLayoutComponent,
+    FooterComponent,
+    HeaderComponent
 
   ],
   imports: [
