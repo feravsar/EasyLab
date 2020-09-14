24ce06e01d91b1958c9cca3419e0e452d5542c5b
TemmuzBurakYavuzer <36274955+TemmuzBurakYavuzer@users.noreply.github.com>
2020-09-14 12:06:56 +0300

    courses style changes

--- apps/web-ui/src/app/modules/teacher/pages/courses/courses.component.ts
+++ apps/web-ui/src/app/modules/teacher/pages/courses/courses.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CourseService } from './../../../../core/services/course.service'
 import { Course } from './../../../../shared/models/Course'
 
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
+  
 })
 export class CoursesComponent implements OnInit {
 
